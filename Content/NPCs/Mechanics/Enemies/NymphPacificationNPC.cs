22707aff760503970a7c5545cec81ef07ec93312
GabeHasWon <gabriel@guevarameyer.com>
2024-08-11 21:09:53 -0500

    lost girl

---
+++ Content/NPCs/Mechanics/Enemies/NymphPacificationNPC.cs
@@ -0,0 +1,25 @@
+﻿using BossForgiveness.Content.NPCs.Vanilla.Enemies;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BossForgiveness.Content.NPCs.Mechanics.Enemies;
+
+internal class NymphPacificationNPC : GlobalNPC
+{
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Nymph || entity.type == NPCID.LostGirl;
+
+    public override bool PreAI(NPC npc)
+    {
+        foreach (var proj in Main.ActiveProjectiles)
+        {
+            if (proj.type == ProjectileID.PurificationPowder && npc.Hitbox.Intersects(proj.Hitbox))
+            {
+                npc.Pacify<LostGirlPacified>();
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Content/NPCs/Vanilla/Enemies/LostGirlPacified.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace BossForgiveness.Content.NPCs.Vanilla.Enemies;
+
+[AutoloadHead]
+public class LostGirlPacified : ModNPC
+{
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[Type] = 16;
+        NPCID.Sets.IsTownPet[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.CloneDefaults(NPCID.LostGirl);
+        NPC.aiStyle = NPCAIStyleID.Passive;
+        NPC.damage = 0;
+        NPC.boss = false;
+        NPC.townNPC = true;
+        NPC.friendly = true;
+        NPC.homeless = true;
+
+        Music = -1;
+        AnimationType = -1;
+    }
+
+    public override void SetChatButtons(ref string button, ref string button2) => button = "";
+    public override string GetChat() => Language.GetTextValue("Mods.BossForgiveness.Dialogue.LostGirl." + Main.rand.Next(4));
+    public override ITownNPCProfile TownNPCProfile() => this.DefaultProfile();
+}
