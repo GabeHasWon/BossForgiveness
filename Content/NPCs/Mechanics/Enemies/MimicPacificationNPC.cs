9c288934415e501d5491d123629f4abd87513ab4
GabeHasWon <gabriel@guevarameyer.com>
2025-01-06 21:25:18 -0600

    mimic progress

---
+++ Content/NPCs/Mechanics/Enemies/MimicPacificationNPC.cs
@@ -0,0 +1,190 @@
+﻿using BossForgiveness.Content.NPCs.Vanilla.Enemies;
+using BossForgiveness.Content.Tiles.Vanilla;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BossForgiveness.Content.NPCs.Mechanics.Enemies;
+
+internal class MimicPacificationNPC : GlobalNPC
+{
+    private const int MaxConvince = 10;
+
+    public override bool InstancePerEntity => true;
+
+    private Point16? chestLoc = null;
+    private bool _hasSeenPlayer = false;
+    private int _convincing = 0;
+    private bool _mimicOpen = true;
+    private bool _goingForChest = false;
+    private float _fakeFrameCounter = 0;
+
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Mimic;
+
+    public override bool PreAI(NPC npc)
+    {
+        MimicPacificationNPC mimic = npc.GetGlobalNPC<MimicPacificationNPC>();
+
+        if (mimic._hasSeenPlayer)
+        {
+            npc.ai[0] = 1;
+            return true;
+        }
+
+        if (_convincing > MaxConvince)
+        {
+            float type = npc.ai[3];
+            npc.Pacify<MimicPacified>();
+            npc.ai[3] = type;
+            return false;
+        }
+
+        Player player = Main.player[npc.target];
+
+        if (Collision.CanHit(npc, player) && player.velocity.LengthSquared() > 3 * 3)
+        {
+            mimic._hasSeenPlayer = true;
+            return true;
+        }
+
+        if (HasMimicChestNearby(npc, out Point16 chestLoc))
+        {
+            if (Vector2.DistanceSquared(npc.Center, chestLoc.ToWorldCoordinates()) > 30 * 30)
+                HopTowardsChest(npc, chestLoc.ToWorldCoordinates());
+            else
+            {
+                npc.velocity.X *= 0.8f;
+
+                int chest = Chest.FindChest(chestLoc.X, chestLoc.Y);
+
+                if (chest != -1)
+                {
+                    int chestFrame = Main.chest[chest].frame;
+
+                    if (chestFrame == 0 && mimic._mimicOpen)
+                    {
+                        mimic._convincing++;
+                        mimic._mimicOpen = false;
+                    }
+                    else if (chestFrame == 2 && !mimic._mimicOpen)
+                    {
+                        mimic._convincing++;
+                        mimic._mimicOpen = true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public override void PostAI(NPC npc)
+    {
+        if (!npc.GetGlobalNPC<MimicPacificationNPC>()._hasSeenPlayer)
+        {
+            npc.ai[0] = 0;
+
+            if (!npc.GetGlobalNPC<MimicPacificationNPC>()._goingForChest)
+            {
+                npc.velocity.X = 0;
+                npc.velocity.Y = MathHelper.Max(npc.velocity.Y, 0);
+            }
+        }
+    }
+
+    public override void FindFrame(NPC npc, int frameHeight)
+    {
+        // Vanilla animation can't run due to the AI suppression, manually redo it
+        if (npc.GetGlobalNPC<MimicPacificationNPC>()._goingForChest)
+        {
+            int frameSpeed = 3;
+            ref float frameCounter = ref npc.GetGlobalNPC<MimicPacificationNPC>()._fakeFrameCounter;
+
+            if (npc.velocity.Y == 0f)
+                frameCounter--;
+            else
+                frameCounter++;
+
+            if (frameCounter < 0.0)
+                frameCounter = 0;
+
+            if (frameCounter > frameSpeed * 4)
+                frameCounter = frameSpeed * 4;
+
+            if (frameCounter < frameSpeed)
+                npc.frame.Y = frameHeight;
+            else if (frameCounter < frameSpeed * 2)
+                npc.frame.Y = frameHeight * 2;
+            else if (frameCounter < frameSpeed * 3)
+                npc.frame.Y = frameHeight * 3;
+            else if (frameCounter < frameSpeed * 4)
+                npc.frame.Y = frameHeight * 4;
+            else if (frameCounter < frameSpeed * 5)
+                npc.frame.Y = frameHeight * 5;
+        }
+    }
+
+    public override void HitEffect(NPC npc, NPC.HitInfo hit) => npc.GetGlobalNPC<MimicPacificationNPC>()._hasSeenPlayer = true;
+
+    private static void HopTowardsChest(NPC npc, Vector2 chestLoc)
+    {
+        if (npc.collideY)
+        {
+            npc.ai[2]++;
+
+            if (npc.ai[2] >= 20)
+            {
+                npc.GetGlobalNPC<MimicPacificationNPC>()._goingForChest = true;
+
+                if (npc.Center.X < chestLoc.X)
+                    npc.velocity.X = 2;
+                else
+                    npc.velocity.X = -2;
+
+                npc.velocity.Y = -3.5f;
+                npc.ai[2] = 0;
+
+                npc.direction = Math.Sign(npc.velocity.X);
+                npc.spriteDirection = npc.direction;
+            }
+            else
+                npc.velocity.X *= 0.9f;
+        }
+    }
+
+    private static bool HasMimicChestNearby(NPC npc, out Point16 chestLoc)
+    {
+        if (npc.GetGlobalNPC<MimicPacificationNPC>().chestLoc is not null)
+        {
+            chestLoc = npc.GetGlobalNPC<MimicPacificationNPC>().chestLoc.Value;
+            return true;
+        }
+
+        const int ScanDistance = 60;
+
+        Point16 center = npc.Center.ToTileCoordinates16();
+
+        for (int i = center.X - ScanDistance; i < center.X + ScanDistance; ++i)
+        {
+            for (int j = center.Y - ScanDistance; j < center.Y + ScanDistance; ++j)
+            {
+                Tile tile = Main.tile[i, j];
+
+                if (tile.HasTile && tile.TileType == ModContent.TileType<MimicChest>())
+                {
+                    chestLoc = new(i, j);
+                    return true;
+                }
+            }
+        }
+
+        chestLoc = Point16.NegativeOne;
+        npc.GetGlobalNPC<MimicPacificationNPC>().chestLoc = chestLoc;
+        return false;
+    }
+}
---
+++ Content/NPCs/Vanilla/Enemies/MimicPacified.cs
@@ -0,0 +1,97 @@
+﻿using BossForgiveness.Content.NPCs.Mechanics.Enemies;
+using System;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace BossForgiveness.Content.NPCs.Vanilla.Enemies;
+
+[AutoloadHead]
+public class MimicPacified : ModNPC
+{
+    public override string Texture => "Terraria/Images/NPC_" + NPCID.Mimic;
+    public override string HeadTexture => (GetType().Namespace + "." + Name).Replace('.', '/') + "_Head";
+
+    public override void SetStaticDefaults()
+    {
+        Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Mimic];
+        NPCID.Sets.IsTownPet[Type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        NPC.CloneDefaults(NPCID.Mimic);
+        NPC.aiStyle = NPCAIStyleID.Passive;
+        NPC.damage = 0;
+        NPC.boss = false;
+        NPC.townNPC = true;
+        NPC.friendly = true;
+        NPC.homeless = true;
+
+        Music = -1;
+        AnimationType = -1;
+    }
+
+    public override bool PreAI()
+    {
+        if (NPC.homeless) 
+        {
+            if (NPC.collideY)
+            {
+                NPC.ai[2]++;
+
+                if (NPC.ai[2] >= 20)
+                {
+                    if (Main.rand.NextBool())
+                        NPC.velocity.X = 2;
+                    else
+                        NPC.velocity.X = -2;
+
+                    NPC.velocity.Y = -3.5f;
+                    NPC.ai[2] = 0;
+
+                    NPC.direction = Math.Sign(NPC.velocity.X);
+                    NPC.spriteDirection = NPC.direction;
+                    NPC.netUpdate = true;
+                }
+                else
+                    NPC.velocity.X *= 0.9f;
+            }
+        }
+
+        return false;
+    }
+
+    public override void FindFrame(int frameHeight)
+    {
+        int frameSpeed = 3;
+
+        if (NPC.collideY)
+            NPC.frameCounter--;
+        else
+            NPC.frameCounter++;
+
+        if (NPC.frameCounter < 0.0)
+            NPC.frameCounter = 0;
+
+        if (NPC.frameCounter > frameSpeed * 4)
+            NPC.frameCounter = frameSpeed * 4;
+
+        if (NPC.frameCounter < frameSpeed)
+            NPC.frame.Y = frameHeight;
+        else if (NPC.frameCounter < frameSpeed * 2)
+            NPC.frame.Y = frameHeight * 2;
+        else if (NPC.frameCounter < frameSpeed * 3)
+            NPC.frame.Y = frameHeight * 3;
+        else if (NPC.frameCounter < frameSpeed * 4)
+            NPC.frame.Y = frameHeight * 4;
+        else if (NPC.frameCounter < frameSpeed * 5)
+            NPC.frame.Y = frameHeight * 5;
+    }
+
+    public override void SetChatButtons(ref string button, ref string button2) => button = "";
+    public override string GetChat() => Language.GetTextValue("Mods.BossForgiveness.Dialogue.Mimic." + Main.rand.Next(4));
+    public override ITownNPCProfile TownNPCProfile() => this.DefaultProfile();
+}
